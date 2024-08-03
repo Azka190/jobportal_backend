b91dbae2f885b1587209e6b8c133fdbd465f6fba
Azka190 <azkarehman2001@gmail.com>
2024-08-03 17:45:48 +0500

    2

--- config/database.js
+++ config/database.js
@@ -1,21 +1,21 @@
 module.exports = ({ env }) => ({
-  defaultconnections: "default",
-      connection:{
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
         client: 'postgres',
-        connection: {
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'prototype'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Azk@.1'),
+        username: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'azk@.1'),
         schema: env('DATABASE_SCHEMA', 'public'),
       },
+      options: {},
     },
+  },
 });
-
-
-
-
 
 
 const path = require('path');
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+   <div>
+    <h1 className="font-extrabold text-red-400">
+      Azka
+    </h1>
+   </div>
+  );
+}
